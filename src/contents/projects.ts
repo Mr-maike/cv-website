59fcf10a8732257403eaf7fcd5aa9b5904a6f74f
Mr-maike <Maike.h.belarmino@gmail.com>
2025-08-31 18:04:05 -0300

    :seed: | Root Files

---
+++ src/contents/projects.ts
@@ -0,0 +1,11 @@
+import { Project } from '@/types';
+
+export const projects:Project[] = [
+    {
+        title: 'Text Editor',
+        description: 'Projeto de Editor de Texto Open Source',
+        technologies: ['Python', 'TKinter', 'PyTest'],
+        githubLink: 'https://github.com',
+        image: '',
+    }
+]
